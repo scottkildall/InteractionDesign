3ef6145fbe3a7efc7f3c3c51801d308a552f78cd
xenoformlabs <xenoformlabs@gmail.com>
2020-02-06 15:37:36 -0800

    added RotateExampleP5

---
+++ RotateExampleP5/sketch.js
@@ -0,0 +1,31 @@
+/*
+  Rotate Example
+  by Scott Kildall
+  
+  Rotation example using P5
+*/
+
+var r = 0;
+
+function setup() {
+  createCanvas(750, 750);
+  ellipseMode(CENTER);
+}
+
+function draw() {
+  background(0);
+
+  // draw an red at center
+  fill(255,0,0);
+
+  // move to origin and perform the translation
+  translate(width/2, height/2);
+  r = r + .01;
+  rotate(r);
+
+  // move to original spot
+  translate(-width/2, -height/2);
+
+  // do actual drawing
+  ellipse( width/2, height/2, 200, 60);
+}
